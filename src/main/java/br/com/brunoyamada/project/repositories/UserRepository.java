ea2c8084e67652e0d53e323c7abe346d329faff1
yamadabruno <bruno_yamada@hotmail.com>
2023-10-22 14:04:13 -0300

    UserRepository

---
+++ src/main/java/br/com/brunoyamada/project/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package br.com.brunoyamada.project.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.brunoyamada.project.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
