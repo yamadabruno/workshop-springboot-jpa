a3f237ab9eddcd04c2ed7d883432823e214ad50e
yamadabruno <bruno_yamada@hotmail.com>
2023-10-22 14:53:13 -0300

    Service layer, component registration

--- src/main/java/br/com/brunoyamada/project/services/UserService.java
+++ src/main/java/br/com/brunoyamada/project/services/UserService.java
@@ -1,6 +1,7 @@
 package br.com.brunoyamada.project.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -16,6 +17,11 @@ public class UserService {
 	
 	public List<User> findAll() {
 		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
 	}
 
 }
