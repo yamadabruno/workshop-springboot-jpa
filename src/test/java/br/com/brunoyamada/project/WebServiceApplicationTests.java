c10551e9daf0bf33cce2f178cabf90abac91f9b5
yamadabruno <bruno_yamada@hotmail.com>
2023-10-22 13:19:42 -0300

    First Commit

---
+++ src/test/java/br/com/brunoyamada/project/WebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.brunoyamada.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
