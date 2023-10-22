8397fc298fe0d6c087bc157004768772ea9eeef8
yamadabruno <bruno_yamada@hotmail.com>
2023-10-22 14:20:40 -0300

    @Autowired UserRepository / Instantiate objects in memory / Persist objects

--- src/main/java/br/com/brunoyamada/project/repositories/UserRepository.java
+++ src/main/java/br/com/brunoyamada/project/repositories/UserRepository.java
@@ -5,5 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import br.com.brunoyamada.project.entities.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+	
+	
 
 }
